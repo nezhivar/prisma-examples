fa44b5655553c45e11962902521f0167dcd6d1d5
aetheode <nezhivar@protonmail.com>
2022-04-26 17:27:56 -0400

    fix the build errors

--- typescript/rest-nextjs-api-routes/pages/api/filterPosts.js
+++ typescript/rest-nextjs-api-routes/pages/api/filterPosts.js
@@ -1,9 +1,8 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
 
 // GET /api/filterPosts?searchString=:searchString
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+export default async function handle(req, res) {
   const { searchString } = req.query
   const resultPosts = await prisma.post.findMany({
     where: {
